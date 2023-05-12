82f04384522ef6b2ea6b6c1bc711d07171883d57
Flan <flannyha@gmail.com>
2023-05-12 15:35:09 +0200

    Initialize OpenGL + separated renderer

---
+++ src/graphics.rs
@@ -0,0 +1,59 @@
+use glfw::{Action, Context, Glfw, Key, Window, WindowEvent};
+use std::sync::mpsc::Receiver;
+
+pub struct Renderer {
+    glfw: Glfw,
+    window: Window,
+    events: Receiver<(f64, WindowEvent)>,
+}
+
+impl Renderer {
+    pub fn new(width: u32, height: u32, title: &str) -> Renderer {
+        // Initialize GLFW
+        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+        // Create window
+        let (mut window, events) = glfw
+            .create_window(width, height, title, glfw::WindowMode::Windowed)
+            .expect("Failed to create window.");
+
+        // Set context to this window
+        glfw.make_context_current(Some(&window));
+        window.set_key_polling(true);
+
+        // Init OpenGL
+        gl::load_with(|f_name| glfw.get_proc_address_raw(f_name));
+
+        // Return a new renderer object
+        Renderer {
+            glfw,
+            window,
+            events,
+        }
+    }
+
+    pub fn should_close(&self) -> bool {
+        self.window.should_close()
+    }
+
+    pub fn begin_frame(&self) {
+        unsafe {
+            gl::ClearColor(0.1, 0.1, 0.2, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
+
+    pub fn end_frame(&mut self) {
+        // Swap front and back buffers
+        self.window.swap_buffers();
+
+        // Poll for and process events
+        self.glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&self.events) {
+            println!("{:?}", event);
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                self.window.set_should_close(true)
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,12 @@
-use glfw::{Action, Context, Key};
+mod graphics;
+use graphics::Renderer;
 
 fn main() {
-    // Initialize GLFW
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-    // Create window
-    let (mut window, events) = glfw
-        .create_window(
-            1920,
-            1080,
-            "FlanRustRenderer (OpenGL)",
-            glfw::WindowMode::Windowed,
-        )
-        .expect("Failed to create window.");
-
-    // Set context to this window
-    window.make_current();
-    window.set_key_polling(true);
+    let mut renderer = Renderer::new(1280, 720, "FlanRustRenderer (OpenGL)");
 
     // Main loop
-    while !window.should_close() {
-        // Swap front and back buffers
-        window.swap_buffers();
-
-        // Poll for and process events
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
-                window.set_should_close(true)
-            }
-        }
+    while !renderer.should_close() {
+        renderer.begin_frame();
+        renderer.end_frame();
     }
 }
